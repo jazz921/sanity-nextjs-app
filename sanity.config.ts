00fc1b38302a66ea069d4306ef028da72e45273b
jdjoaquin <joaquinjdj78@gmai.com>
2025-03-27 20:27:13 +0800

    added sanity studio setup, added preconfigure schemas, fixed missing dependencies

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+'use client'
 
-const config = defineConfig({
-  projectId: 'ydmv7k8f',
-  dataset: 'production',
-  title: 'Personal Website',
-  apiVersion: '2025-3-27',
-  basePath: '/admin',
-  plugins: [deskTool()]
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './src/sanity/env'
+import {schema} from './src/sanity/schemaTypes'
+import {structure} from './src/sanity/structure'
+
+export default defineConfig({
+  title: "Cake",
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema,
+  plugins: [
+    structureTool({structure}),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
 })
-
-export default config
