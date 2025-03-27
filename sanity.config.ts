be8c263ead919bc8c125b90f179ad036e80c5f07
Justine Dave <justin.joaquin@ext.essilor.com>
2025-03-27 17:58:09 +0800

    initial set up for nextjs and sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "sanity";
+
+const config = defineConfig({
+  projectId: 'ydmv7k8f',
+  dataset: 'production',
+  title: 'Personal Website',
+  apiVersion: '2025-3-27',
+  basePath: ''
+})
+
+export default config
