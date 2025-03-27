13defbd1d14cc9287436d4b3cf428f67b35d10cd
jdjoaquin <joaquinjdj78@gmai.com>
2025-03-27 18:57:33 +0800

    clean up

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
 
 const config = defineConfig({
   projectId: 'ydmv7k8f',
   dataset: 'production',
   title: 'Personal Website',
   apiVersion: '2025-3-27',
-  basePath: ''
+  basePath: '/admin',
+  plugins: [deskTool()]
 })
 
 export default config
